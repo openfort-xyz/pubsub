9b00056a37ba09931e58a1dbf2af2cdc085130ba
mikelsr <mikelsr@pm.me>
2025-05-15 16:12:32 +0200

    Run gofmt on errors.go

--- rabbit/errors.go
+++ rabbit/errors.go
@@ -3,8 +3,8 @@ package rabbit
 import "errors"
 
 var (
-	ErrConnClosed = errors.New("rabbitmq connection is closed")
-	ErrChanClosed = errors.New("rabbitmq channel is closed")
-	ErrChanNil = errors.New("rabbitmq channel is nil, please call Connect first")
+	ErrConnClosed        = errors.New("rabbitmq connection is closed")
+	ErrChanClosed        = errors.New("rabbitmq channel is closed")
+	ErrChanNil           = errors.New("rabbitmq channel is nil, please call Connect first")
 	ErrHealthCheckFailed = errors.New("rabbitmq channel health check failed")
 )
